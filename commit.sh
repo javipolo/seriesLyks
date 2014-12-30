e2bc9e9bce93554f6fde8e1ca462153c4ffdd609
Victor Perez <viperey@gmail.com>
2014-12-30 01:35:50 +0100

    Leftovers

---
+++ commit.sh
@@ -0,0 +1,4 @@
+git add ddbb/tvPrograms/ && git commit -m 'tvPrograms' && echo Programs
+git add ddbb/movies/ && git commit -m 'movies' && echo movies
+git add ddbb/series/ && git commit -m 'series' && echo series
+git push
